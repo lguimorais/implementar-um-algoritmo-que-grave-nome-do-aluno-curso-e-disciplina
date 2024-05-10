c7c10c25719c2b6f8e0ad4b386dbb0167618bda6
luis guilherme <tete42449@gmail.com>
2024-05-09 23:39:41 -0300

    codigo fim

--- algoritmo.c
+++ algoritmo.c
@@ -5,25 +5,48 @@ FILE *arquivo;
 char nome[20];
 char curso[20];
 char disciplina[20];
-int main()
+void algoritmo()
 {
 
     arquivo = fopen("C:\\Users\\tete4\\Documents\\implementar-um-algoritmo-que-grave-nome-do-aluno-curso-e-disciplina\\algoritmo.txt", "w+");
     if (arquivo == 0)
     {
         printf("falha ao visualizar o arquivo.\n");
-    } else{
+    }
+    else
+    {
         printf("arquivo encontrado com sucesso\n");
         system("pause");
         system("cls");
         printf("informe seu nome:\n");
         scanf("%s", &nome);
         printf("informe seu curso:\n");
-        scanf("%s",&curso);
+        scanf("%s", &curso);
         printf("informe a materia:\n");
-        scanf("%s",&disciplina);
-        fprintf("");
-    
-        
+        scanf("%s", &disciplina);
+        fprintf(arquivo, "o nome do aluno e:%s\n", nome);
+        fprintf(arquivo, "a disciplina e:%s\n", disciplina);
+        fprintf(arquivo, "o curso e:%s\n", curso);
+        fclose(arquivo);
+        system("pause");
     }
+}
+int main()
+
+{
+    algoritmo();
+    FILE *arquivo;
+    char linha[100];
+    arquivo = fopen("C:\\Users\\tete4\\Documents\\implementar-um-algoritmo-que-grave-nome-do-aluno-curso-e-disciplina\\algoritmo.txt", "r");
+    if (arquivo == NULL)
+    {
+        printf("erro ao abrir o arquivo\n");
+        return 1;
+    }
+    while (fgets(linha, sizeof(linha), arquivo) != NULL)
+    {
+
+        printf("%s", linha);
+    }
+    fclose(arquivo);
 }
